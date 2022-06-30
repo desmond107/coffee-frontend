a4dcc6f10b585f71009302574a509f7f647adb64
desmond <desmond@desmonds-MacBook-Pro.local>
2022-06-30 14:51:30 +0300

    1

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='hero'>
+        <div class="card bg-dark text-white">
+  <img src="/assets/cofe1.jpeg" class="card-img" alt="Background" height="700px"/>
+  <div class="card-img-overlay d-flex flex-column justify-content-center">
+    <div className="container"></div>
+    <h5 class="card-title display-3" fw-bolder mb-0>NEW BARISTA SPECIAL</h5>
+    <p class="card-text">
+      KAHAWA KAMA DAWA
+      </p>
+    
+  </div>
+</div>
+        </div>
+  )
+}
+
+export default Home
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/font-awesome/dist/css/font-awesome.min.css'
+ReactDOM.render(
+    <React.StrictMode>
+<App />,
+    </React.StrictMode>,
+ 
+document.getElementById("root")
+);
