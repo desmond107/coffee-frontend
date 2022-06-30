5e1a9d54431cfbc98db2e3a5fc6e60aa486722cf
desmond <desmond@desmonds-MacBook-Pro.local>
2022-06-30 17:53:36 +0300

    2

--- src/components/prodducts.jsx
+++ src/components/prodducts.jsx
@@ -26,14 +26,43 @@ const Prodducts = () => {
           getProducts();
        },[]);
 
+       const Loading = () => {
+
+        return( <>
+        Loading....
+        </>
+        )
+       }
+       const ShowProducts = () => {
+
+        return(
+            <>
+            <div className='buttons d-flex justify-content-center mp-5 pb-5'>
+            <button className='btn btn-outline-dark'>All</button>
+            <button className='btn btn-outline-dark'>Coffee Beans</button>
+            <button className='btn btn-outline-dark'>Ground Coffee</button>
+            <button className='btn btn-outline-dark'>Take-away Coffee</button>
+            <button className='btn btn-outline-dark'>Coffee-Blends</button>
+            <button className='btn btn-outline-dark'>Hybrid coffee</button>
+            
+        </div>
+            </>
+        )
+
+        
+       }
+
      return (
          <div>
             <div className='container my-5 py-5'>
                 <div className='row'>
                     <div className='col-12 mb-5'>
-                        <h1 className='display-6 fw-bolder text-center'>Coffee Beans</h1>
+                        <h1 className='display-6 fw-bolder text-center'>Our Products</h1>
                         <hr/>
                     </div>
+                </div>
+                <div className='row justify-content-center'>
+                    {loading? <Loading/> : <ShowProducts/> }
                 </div>
             </div>
         </div>
